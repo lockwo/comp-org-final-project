e0578191a0d0290525397c772138a1c322e76bb0
wandyrandy <42878069+wandyrandy@users.noreply.github.com>
2019-12-09 18:00:50 -0500

    instruction class created

---
+++ instruction.py
@@ -0,0 +1,20 @@
+class Instruction:
+
+    def __init__(self, instruct):
+        self.counter = 0
+        self.instruct = instruct
+        if len(instruct) > 3:
+            instruction = instruct.split(' ')
+            self.oper = instruction[0]
+            self.r1 = instruction[1].split(',')[0]
+            self.r2 = instruction[1].split(',')[1]
+            self.r3 = instruction[1].split(',')[2]
+        else:
+            self.oper = 'nop'
+            self.r1 = 0
+            self.r2 = 0
+            self.r3 = 0
+
+    def __str__(self):
+        return f'{self.instruct}\n{self.oper}\n{self.r1}\n{self.r2}\n{self.r3}\n{self.counter}\n'
+    
