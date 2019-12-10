a74116b371e27a130057eac472f52ae9d7c648a9
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2019-12-10 14:09:58 -0500

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,15 +1,22 @@
 import sys
+from file_read import file_read
 
 if __name__=='__main__':
+    '''
+    THIS IS JUST COMMENTED OUT BECAUSE IM LAZY AND DONT WANT TO RUN FROM COMMAND LINE
     forward = sys.argv[1]
 
     try:
         input_file = open(sys.argv[2], 'r')
     except:
         print('Failed to open file\n')
-
+        sys.exit()
+    '''
+    input_file = open("p1-input01.txt")
     s = []
     t = []
-    instructs = []
-    # ADD FUNCTION TO PARSE INSTRUCTIONS INTO THE INSTRUCTS LIST
-    # ADD FOR LOOP FOR CPU CYCLES
+    instructs, all_instructs = file_read(input_file)
+
+    print(instructs, all_instructs)
+    for i in instructs:
+        print(i)
