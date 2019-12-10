418a7e508e97b3ddd6b194b8b15d0c548c191b62
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2019-12-10 17:01:24 -0500

    Update file_read.py

--- file_read.py
+++ file_read.py
@@ -3,9 +3,11 @@ from instruction import Instruction
 def file_read(f):
     full = []
     instruct = []
+    i = 0
     for line in f:
         full.append(line.strip())
         if ':' not in line:
-            instruct.append(Instruction(line.strip()))
+            instruct.append(Instruction(line.strip()), i)
+            i += 1
 
     return instruct, full
