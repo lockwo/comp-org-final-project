a1beac1c5b4d870da4bdff0799291dd8c94d9d21
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2019-12-10 14:12:30 -0500

    Update p1.py

--- p1.py
+++ p1.py
@@ -2,8 +2,6 @@ import sys
 from file_read import file_read
 
 if __name__=='__main__':
-    '''
-    THIS IS JUST COMMENTED OUT BECAUSE IM LAZY AND DONT WANT TO RUN FROM COMMAND LINE
     forward = sys.argv[1]
 
     try:
@@ -11,10 +9,10 @@ if __name__=='__main__':
     except:
         print('Failed to open file\n')
         sys.exit()
-    '''
-    input_file = open("p1-input01.txt")
+    
     s = []
     t = []
+    # all_instructs is just list of strings of the file per line, mainly just for Randy/Branching, feel free to modify it how you see fit
     instructs, all_instructs = file_read(input_file)
 
     print(instructs, all_instructs)
