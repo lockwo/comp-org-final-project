8ededb4f1351304f1e47c5e0c18474a349bf2d19
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2019-12-10 15:19:36 -0500

    Update print_cpu.py

--- print_cpu.py
+++ print_cpu.py
@@ -1,6 +1,8 @@
 from instruction import Instruction
 
 def print_cpu(instructs, s, t):
+    print("----------------------------------------------------------------------------------")
+    print("CPU Cycles ===>\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12\t13\t14\t15\t16")
     for i in instructs:
         print(i)
     
@@ -13,4 +15,6 @@ def print_cpu(instructs, s, t):
         print("$t" + str(i), "=", str(t[i]) + "\t", end='') 
         if i == 3 or i == 7:
             print()
+    
+    print()
     
