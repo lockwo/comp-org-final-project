3848e80349f61a7ea05b586786d463102955e9fd
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2019-12-10 17:32:15 -0500

    Update print_cpu.py

--- print_cpu.py
+++ print_cpu.py
@@ -4,10 +4,10 @@ def print_cpu(instructs, s, t, cycle):
     print("----------------------------------------------------------------------------------")
     print("CPU Cycles ===>\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12\t13\t14\t15\t16")
     for i in instructs:
-        if (i.counter == 0) or (i.order < cycle):
+        if (i.counter == 0) or (i.order > cycle):
             continue
-        print(i.oper, end='\t')
-        for j in range(i.order):
+        print(i.instruct, end='\t')
+        for j in range(i.order-1):
             print(".", end='\t')
         for j in range(i.counter):
             print(i.pipe[j], end='\t')
