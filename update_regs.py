ba9c40ca8cb66a653ed268a63ea1d4c955e8cdd8
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2019-12-10 15:19:00 -0500

    Update update_regs.py

--- update_regs.py
+++ update_regs.py
@@ -1,5 +1,129 @@
 from instruction import Instruction
 
 
-def update_regs(instructs, total_cycles, s, t):
+def update_regs(instructs, s, t):
+    for i in range(len(instructs)):
+        if instructs[i].counter != 5:
+            continue
+        if instructs[i].oper == "add":
+            if instructs[i].r2[1] == "s":
+                v1 = s[int(instructs[i].r2[2])]
+            elif instructs[i].r2[1] == "t":
+                v1 = t[int(instructs[i].r2[2])]
+            else:
+                v1 = 0
+            if instructs[i].r3[1] == "s":
+                v2 = s[int(instructs[i].r3[2])]
+            elif instructs[i].r3[1] == "t":
+                v2 = t[int(instructs[i].r3[2])]
+            else:
+                v2 = 0
+            vale = v1 + v2
+            if instructs[i].r1[1] == "s":
+                s[int(instructs[i].r1[2])] = vale
+            else:
+                t[int(instructs[i].r1[2])] = vale
+        elif instructs[i].oper == "addi":
+            if instructs[i].r2[1] == "s":
+                v1 = s[int(instructs[i].r2[2])]
+            elif instructs[i].r2[1] == "t":
+                v1 = t[int(instructs[i].r2[2])]
+            else:
+                v1 = 0
+            vale = v1 + int(instructs[i].r3)
+            if instructs[i].r1[1] == "s":
+                s[int(instructs[i].r1[2])] = vale
+            else:
+                t[int(instructs[i].r1[2])] = vale
+        elif instructs[i].oper == "and":
+            if instructs[i].r2[1] == "s":
+                v1 = s[int(instructs[i].r2[2])]
+            elif instructs[i].r2[1] == "t":
+                v1 = t[int(instructs[i].r2[2])]
+            else:
+                v1 = 0
+            if instructs[i].r3[1] == "s":
+                v2 = s[int(instructs[i].r3[2])]
+            elif instructs[i].r3[1] == "t":
+                v2 = t[int(instructs[i].r3[2])]
+            else:
+                v2 = 0
+            vale = v1 & v2
+            if instructs[i].r1[1] == "s":
+                s[int(instructs[i].r1[2])] = vale
+            else:
+                t[int(instructs[i].r1[2])] = vale
+        elif instructs[i].oper == "andi":
+            if instructs[i].r2[1] == "s":
+                v1 = s[int(instructs[i].r2[2])]
+            elif instructs[i].r2[1] == "t":
+                v1 = t[int(instructs[i].r2[2])]
+            else:
+                v1 = 0
+            vale = v1 & int(instructs[i].r3)
+            if instructs[i].r1[1] == "s":
+                s[int(instructs[i].r1[2])] = vale
+            else:
+                t[int(instructs[i].r1[2])] = vale
+        elif instructs[i].oper == "or":
+            if instructs[i].r2[1] == "s":
+                v1 = s[int(instructs[i].r2[2])]
+            elif instructs[i].r2[1] == "t":
+                v1 = t[int(instructs[i].r2[2])]
+            else:
+                v1 = 0
+            if instructs[i].r3[1] == "s":
+                v2 = s[int(instructs[i].r3[2])]
+            elif instructs[i].r3[1] == "t":
+                v2 = t[int(instructs[i].r3[2])]
+            else:
+                v2 = 0
+            vale = v1 | v2
+            if instructs[i].r1[1] == "s":
+                s[int(instructs[i].r1[2])] = vale
+            else:
+                t[int(instructs[i].r1[2])] = vale
+        elif instructs[i].oper == "ori":
+            if instructs[i].r2[1] == "s":
+                v1 = s[int(instructs[i].r2[2])]
+            elif instructs[i].r2[1] == "t":
+                v1 = t[int(instructs[i].r2[2])]
+            else:
+                v1 = 0
+            vale = v1 | int(instructs[i].r3)
+            if instructs[i].r1[1] == "s":
+                s[int(instructs[i].r1[2])] = vale
+            else:
+                t[int(instructs[i].r1[2])] = vale
+        elif instructs[i].oper == "slt":
+            if instructs[i].r2[1] == "s":
+                v1 = s[int(instructs[i].r2[2])]
+            elif instructs[i].r2[1] == "t":
+                v1 = t[int(instructs[i].r2[2])]
+            else:
+                v1 = 0
+            if instructs[i].r3[1] == "s":
+                v2 = s[int(instructs[i].r3[2])]
+            elif instructs[i].r3[1] == "t":
+                v2 = t[int(instructs[i].r3[2])]
+            else:
+                v2 = 0
+            vale = v1 < v2
+            if instructs[i].r1[1] == "s":
+                s[int(instructs[i].r1[2])] = vale
+            else:
+                t[int(instructs[i].r1[2])] = vale
+        elif instructs[i].oper == "slti":
+            if instructs[i].r2[1] == "s":
+                v1 = s[int(instructs[i].r2[2])]
+            elif instructs[i].r2[1] == "t":
+                v1 = t[int(instructs[i].r2[2])]
+            else:
+                v1 = 0
+            vale = v1 < int(instructs[i].r3)
+            if instructs[i].r1[1] == "s":
+                s[int(instructs[i].r1[2])] = vale
+            else:
+                t[int(instructs[i].r1[2])] = vale
+
     return s, t
