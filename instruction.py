4fb37cf233c3b4e11d3dd60b1ea765c71bea3929
MackQian <37890170+MackQian@users.noreply.github.com>
2019-12-10 16:43:48 -0500

    ADDED A THING TO STORE THE CLOCK CYCLES
    needed for nops because nops take the stuff that is in the instruction that needs it. Might also make it easier to print stuff

--- instruction.py
+++ instruction.py
@@ -4,6 +4,7 @@ class Instruction:
         self.counter = 0
         self.instruct = instruct
         self.taken = False
+        self.cycles=['.']*16
         if len(instruct) > 3:
             instruction = instruct.split(' ')
             self.oper = instruction[0]
@@ -15,7 +16,6 @@ class Instruction:
             self.r1 = 0
             self.r2 = 0
             self.r3 = 0
-
     def __str__(self):
         return f'{self.instruct}\n{self.oper}\n{self.r1}\n{self.r2}\n{self.r3}\n{self.counter}\n'
     
