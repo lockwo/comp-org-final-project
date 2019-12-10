96c996a9239e83b08f7417ed246762c32bbf7cb8
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2019-12-10 17:27:02 -0500

    Update forward.py

--- forward.py
+++ forward.py
@@ -1,5 +1,8 @@
 from instruction import Instruction
 
-
+# JUST A BASE CASE IMPLEMENTATION SO I CAN TEST THINGS USING THIS
 def forward(instructs, total_cycles):
+    for i in instructs:
+        if (i.counter == 0 and i.order == total_cycles) or (i.counter != 0 and i.counter != 5):
+            i.counter += 1
     return instructs
