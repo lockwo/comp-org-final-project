c2d0778b4e552f7b2d39dd33344284e6787c8961
MackQian <37890170+MackQian@users.noreply.github.com>
2019-12-11 03:15:01 -0500

    Update p1.py

--- p1.py
+++ p1.py
@@ -16,7 +16,7 @@ if __name__=='__main__':
     except:
         print('Failed to open file\n')
         sys.exit()
-    
+
     s = [0 for i in range(8)]
     t = [0 for i in range(10)]
     # all_instructs is just list of strings of the file per line, mainly just for Randy/Branching, feel free to modify it how you see fit
@@ -28,12 +28,15 @@ if __name__=='__main__':
     '''
     done = False
     total_cycles = 1
+    toprint=0
     if f == "F":
         print("START OF SIMULATION (forwarding)")
     else:
         print("START OF SIMULATION (no forwarding)")
     while ((not done) and (total_cycles <= 16)):
         instructs = branch(instructs, all_instructs, total_cycles, t, s)
+        #for j in instructs:
+        #    print(j.instruct,j.taken,j.counter,j.taken_counter)
         if f == "F":
             instructs = forward(instructs, total_cycles)
         else:
@@ -41,12 +44,11 @@ if __name__=='__main__':
         s, t = update_regs(instructs, s, t)
         print_cpu(instructs, s, t, total_cycles)
         done = True
-        done = True
         for i in instructs:
             if i.counter != 6:
                 done = False
-        if(done == False and instructs[len(instructs)-1].counter == 5):
-            done = True
+        if(done==False and instructs[len(instructs)-1].counter==5):
+            done=True
         total_cycles += 1
 
     print("----------------------------------------------------------------------------------")
