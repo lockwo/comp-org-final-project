96a9bbc4138985cdb5321c7c7e14a3bd969bd5fa
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2019-12-11 12:09:57 -0500

    Update print_cpu.py

--- print_cpu.py
+++ print_cpu.py
@@ -10,13 +10,17 @@ def print_cpu(instructs, s, t, cycle):
                 print(c,end='\t')
             print()
 
+    # from https://stackoverflow.com/questions/8450472/how-to-print-a-string-in-fixed-width
     for i in range(len(s)):
-        print("$s" + str(i), "=", str(s[i]) + "\t", end='')
+        #print("I", i)
+        st = "$s" + str(i) + " = " + str(s[i])
+        print(st.ljust(20), end='')
         if i == 3 or i == 7:
             print()
 
     for i in range(len(t)):
-        print("$t" + str(i), "=", str(t[i]) + "\t", end='')
+        st = "$t" + str(i) + " = " + str(t[i])
+        print(st.ljust(20), end='')
         if i == 3 or i == 7:
             print()
 
