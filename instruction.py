ec810fce366c7f70e6a832d887573d9259178458
MackQian <37890170+MackQian@users.noreply.github.com>
2019-12-11 00:01:14 -0500

    Update instruction.py

--- instruction.py
+++ instruction.py
@@ -12,9 +12,14 @@ class Instruction:
         if len(instruct) > 3:
             instruction = instruct.split(' ')
             self.oper = instruction[0]
-            self.r1 = instruction[1].split(',')[0]
-            self.r2 = instruction[1].split(',')[1]
-            self.r3 = instruction[1].split(',')[2]
+            if(self.oper=="beq" or self.oper=="bne"):
+                self.r2 = instruction[1].split(',')[0]
+                self.r3 = instruction[1].split(',')[1]
+                self.r1 = instruction[1].split(',')[2]
+            else:
+                self.r1 = instruction[1].split(',')[0]
+                self.r2 = instruction[1].split(',')[1]
+                self.r3 = instruction[1].split(',')[2]
         else:
             self.oper = 'nop'
             self.r1 = 0
